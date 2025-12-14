50f1ea3533dc2d1c4dcd10a7542143cd8c3601c5
Drew Albert <drewalbert7@gmail.com>
2025-12-14 02:59:04 +0000

    Change notification icon to standard bell icon like Reddit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -116,7 +116,7 @@ const Header: React.FC<HeaderProps> = ({ isMobileSidebarOpen, onMobileSidebarTog
                     className="p-2 text-black hover:text-gray-600 transition-colors"
                   >
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth={2}>
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-5 5v-5zM4.5 19.5a2.5 2.5 0 01-2.5-2.5V6a2.5 2.5 0 012.5-2.5h15A2.5 2.5 0 0122 6v11a2.5 2.5 0 01-2.5 2.5h-15z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                     </svg>
                     {/* Notification dot */}
                     <div className="absolute -bottom-1 -right-1 w-2 h-2 bg-black rounded-full"></div>
